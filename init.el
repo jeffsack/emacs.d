9d0e78da876021f305f1f7a359053e8a780e7a83
Jeff Sack <jeff.sack@gmail.com>
2013-10-07 16:34:37 -0500

    initial emacs.d commit

---
+++ init.el
@@ -0,0 +1,17 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; Add in your own as you wish:
+(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-eshell starter-kit-bindings clojure-mode clojure-test-mode nrepl projectile magit)
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+(global-auto-revert-mode)
