9b04b00a4faca13e3fdaf91a6414c3230449ab77
Jeff Sack <jeff.sack@quantumretail.com>
2013-10-08 00:18:15 -0500

    using cyberpunk theme, setting up ac and docs for nrepl

--- init.el
+++ init.el
@@ -1,13 +1,17 @@
 (require 'package)
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
+
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
 (package-initialize)
 
 (when (not package-archive-contents)
   (package-refresh-contents))
 
 ;; Add in your own as you wish:
-(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-eshell starter-kit-bindings clojure-mode clojure-test-mode nrepl projectile magit)
+(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-eshell starter-kit-bindings clojure-mode clojure-test-mode nrepl projectile magit ac-nrepl rainbow-delimiters)
   "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
@@ -15,3 +19,29 @@
     (package-install p)))
 
 (global-auto-revert-mode)
+
+(load-theme 'cyberpunk t)
+
+
+;; Configure nrepl.el
+;(setq nrepl-hide-special-buffers t)
+;(setq nrepl-popup-stacktraces-in-repl t)
+(setq nrepl-history-file "~/.emacs.d/nrepl-history")
+ 
+;; Some default eldoc facilities
+(add-hook 'nrepl-connected-hook
+          (defun pnh-clojure-mode-eldoc-hook ()
+            (add-hook 'clojure-mode-hook 'turn-on-eldoc-mode)
+            (add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
+            (nrepl-enable-on-existing-clojure-buffers)))
+
+;; Repl mode hook
+;(add-hook 'nrepl-mode-hook 'subword-mode)
+
+
+;; Auto completion for NREPL
+(require 'ac-nrepl)
+(eval-after-load "auto-complete"
+  '(add-to-list 'ac-modes 'nrepl-mode))
+(add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
+ 
