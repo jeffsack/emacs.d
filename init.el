79692abde174ebc00ff1924ccd683f73d07f5c0e
Jeff Sack <jeff.sack@gmail.com>
2013-10-08 22:24:55 -0500

    another attempt at fixing PATH

--- init.el
+++ init.el
@@ -58,8 +58,11 @@
             (ns-raise-emacs)))))
 
 
-(if (not (getenv "TERM_PROGRAM"))
-      (let ((path (shell-command-to-string
-              "$SHELL -cl \"printf %s \\\"\\\$PATH\\\"\"")))
-        (setenv "PATH" path)))
+;; fix the PATH variable
+(defun set-exec-path-from-shell-PATH ()
+  (let ((path-from-shell (shell-command-to-string "$SHELL -i -c 'echo $PATH'")))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+(when window-system (set-exec-path-from-shell-PATH))
 
